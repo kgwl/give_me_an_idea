5be6c260c455557c0ce1a04d313ae99fe048fba8
kgwl <kacper.gawle@protonmail.com>
2021-08-03 12:19:14 +0200

    Created idea generator template with submit button

--- give_me_an_idea/settings.py
+++ give_me_an_idea/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'idea_generator'
 ]
 
 MIDDLEWARE = [
--- give_me_an_idea/urls.py
+++ give_me_an_idea/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from idea_generator.views import index_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',index_view,name='index')
+
 ]
