711c1f69738595887afe63c91dfa5078aa1d995a
kgwl <kacper.gawle@protonmail.com>
2021-08-14 23:19:26 +0200

    Created idea model

--- idea_generator/models.py
+++ idea_generator/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Idea(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=200,default='default')
+    difficulty = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
