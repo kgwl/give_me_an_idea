d743a93990f1b1eaeec47667aad3166521335de5
kgwl <kacper.gawle@protonmail.com>
2021-11-08 12:15:39 +0100

    New model field has been added. Difficulties model - stores data with difficulty levels

--- idea_generator/models.py
+++ idea_generator/models.py
@@ -5,10 +5,13 @@ class Category(models.Model):
     id = models.AutoField(primary_key=True)
     category = models.CharField(max_length=200)
 
+class Difficulties(models.Model):
+    id = models.AutoField(primary_key=True)
+    difficulty = models.CharField(max_length=100)
 
 class Idea(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
-    difficulty = models.CharField(max_length=100)
+    difficulty = models.ForeignKey(Difficulties,on_delete=models.PROTECT)
     description = models.TextField(max_length=500)
