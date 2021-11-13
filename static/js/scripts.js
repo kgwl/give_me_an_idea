a266dc4e6c2419d17ea770fb51bfed64db07e61e
kgwl <kacper.gawle@protonmail.com>
2021-11-13 18:27:26 +0100

    New html code compatible with bootstrap was created in base.html,main.html files

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,32 +1,32 @@
-/*
-* Function resizeToFit() has been taken from:
-* https://stackoverflow.com/questions/18229230/dynamically-changing-the-size-of-font-size-based-on-text-length-using-css-and-ht
-*
-* */
-$(document).ready(function (){
-    resizeToFit();
-    marginToFit();
-
-})
-
-function resizeToFit(){
-    var fontSize = $("#description").css("font-size");
-    $("#description").css("font-size",parseFloat(fontSize) - 2);
-    if ($("#description").height() >= $("#output").height() - $(".container-header").height()){
-        resizeToFit();
-    }
-}
-
-function marginToFit(){
-    var classItems = $(".container");
-    var outputHeight = $("#output").height();
-
-    for (i = 0;i<classItems.length;i++){
-        item = $("#"+classItems[i].id);
-        itemHeight = item.height();
-        itemHeight = parseInt((outputHeight - itemHeight)/3);
-        item.css("margin-top",itemHeight);
-    }
-
-}
+// /*
+// * Function resizeToFit() has been taken from:
+// * https://stackoverflow.com/questions/18229230/dynamically-changing-the-size-of-font-size-based-on-text-length-using-css-and-ht
+// *
+// * */
+// $(document).ready(function (){
+//     resizeToFit();
+//     marginToFit();
+//
+// })
+//
+// function resizeToFit(){
+//     var fontSize = $("#description").css("font-size");
+//     $("#description").css("font-size",parseFloat(fontSize) - 2);
+//     if ($("#description").height() >= $("#output").height() - $(".container-header").height()){
+//         resizeToFit();
+//     }
+// }
+//
+// function marginToFit(){
+//     var classItems = $(".container");
+//     var outputHeight = $("#output").height();
+//
+//     for (i = 0;i<classItems.length;i++){
+//         item = $("#"+classItems[i].id);
+//         itemHeight = item.height();
+//         itemHeight = parseInt((outputHeight - itemHeight)/3);
+//         item.css("margin-top",itemHeight);
+//     }
+//
+// }
 
