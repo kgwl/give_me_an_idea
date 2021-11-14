1c0b340dca13c52936fcdb7dd4231703f636aee3
kgwl <kacper.gawle@protonmail.com>
2021-11-14 13:15:20 +0100

    Function paddingToFit has been added to script.js file. This function set text on the center of the .output-row div.Other function has been removed.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,32 +1,35 @@
-// /*
-// * Function resizeToFit() has been taken from:
-// * https://stackoverflow.com/questions/18229230/dynamically-changing-the-size-of-font-size-based-on-text-length-using-css-and-ht
-// *
-// * */
-// $(document).ready(function (){
-//     resizeToFit();
-//     marginToFit();
-//
-// })
-//
-// function resizeToFit(){
-//     var fontSize = $("#description").css("font-size");
-//     $("#description").css("font-size",parseFloat(fontSize) - 2);
-//     if ($("#description").height() >= $("#output").height() - $(".container-header").height()){
-//         resizeToFit();
-//     }
-// }
-//
-// function marginToFit(){
-//     var classItems = $(".container");
-//     var outputHeight = $("#output").height();
-//
-//     for (i = 0;i<classItems.length;i++){
-//         item = $("#"+classItems[i].id);
-//         itemHeight = item.height();
-//         itemHeight = parseInt((outputHeight - itemHeight)/3);
-//         item.css("margin-top",itemHeight);
-//     }
-//
-// }
+$(document).ready(function () {
+    paddingToFit();
+})
 
+$(window).resize(function () {
+    paddingToFit();
+})
+
+function paddingToFit() {
+    var windowWidth = $(window).width();
+
+    if (windowWidth >= 992) {
+        switchHeight(1);
+    } else {
+        if (item.css("margin-top") != "0px") {
+            switchHeight(0)
+        }
+    }
+}
+
+function switchHeight(value) {
+    var outputItems = $(".output");
+    var outputHeight = $(".output-row").height();
+
+    for (i = 0; i < outputItems.length; i++) {
+        item = $("#" + outputItems [i].id);
+        var itemHeight = item.height();
+        if (value == 1) {
+            itemHeight = parseInt((outputHeight - itemHeight) / 4);
+        } else {
+            itemHeight = "auto";
+        }
+        item.css("margin-top", itemHeight);
+    }
+}
