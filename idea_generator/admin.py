81661a782e17b47b8ec95657a59ef9252fc5c803
kgwl <kacper.gawle@protonmail.com>
2021-11-30 20:06:46 +0100

    Display Idea model in django admin has been changed.

--- idea_generator/admin.py
+++ idea_generator/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Idea, Category, Difficulties
 
-admin.site.register(Idea)
 admin.site.register(Category)
 admin.site.register(Difficulties)
+
+
+class IdeaAdmin(admin.ModelAdmin):
+    list_display = ('name','category','difficulty')
+
+admin.site.register(Idea,IdeaAdmin)
